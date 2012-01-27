faf947276cd5dee858e4c64c884c14d70b84d09f
aphethean <aphethean@temenos.com>
2012-01-27 10:36:42 +0000

    Updated prototype.

--- interaction-core/src/main/java/com/temenos/interaction/core/link/ResourceState.java
+++ interaction-core/src/main/java/com/temenos/interaction/core/link/ResourceState.java
@@ -1,6 +1,8 @@
 package com.temenos.interaction.core.link;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 import com.temenos.interaction.core.state.ResourceInteractionModel;
@@ -8,8 +10,15 @@ import com.temenos.interaction.core.state.ResourceInteractionModel;
 public class ResourceState {
 
 	public ResourceInteractionModel resource;
-	public Set<Transition> transitions = new HashSet<Transition>();
 	
+	private String name;
+//	  private List<Command> actions = new ArrayList<Command>();
+	private Map<String, Transition> transitions = new HashMap<String, Transition>();
+	  public void addTransition(Eval eval, ResourceState targetState) {
+	    assert null != targetState;
+	    transitions.put(eval.getName(), new Transition(this, eval, targetState));
+	  }
+
 	public ResourceState(ResourceInteractionModel resource) {
 		this.resource = resource;
 	}
--- interaction-core/src/test/java/com/temenos/interaction/core/link/TestResourceStateMachine.java
+++ interaction-core/src/test/java/com/temenos/interaction/core/link/TestResourceStateMachine.java
@@ -2,6 +2,8 @@ package com.temenos.interaction.core.link;
 
 import static org.junit.Assert.*;
 
+import org.junit.Test;
+
 public class TestResourceStateMachine {
 
 	@Test
