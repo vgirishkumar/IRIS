113accdd9427bd2a0db7adeefd53f3993ce45cf0
aphethean <aphethean@temenos.com>
2012-03-07 11:17:02 +0000

    Added NoopGETCommand.java, useful when trying to stub out a few GET commands to the CommandController just to get things working.

---
+++ interaction-core/src/main/java/com/temenos/interaction/core/command/NoopGETCommand.java
@@ -0,0 +1,17 @@
+package com.temenos.interaction.core.command;
+
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.Response;
+
+import com.temenos.interaction.core.EntityResource;
+import com.temenos.interaction.core.RESTResponse;
+
+public final class NoopGETCommand implements ResourceGetCommand {
+
+	@Override
+	public RESTResponse get(String id,
+			MultivaluedMap<String, String> queryParams) {
+		return new RESTResponse(Response.Status.OK, new EntityResource(""));
+	}
+	
+}
