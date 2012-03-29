1344316479c326014e580e9ea5dbbd3cb6c65bbc
aphethean <aphethean@temenos.com>
2012-03-29 21:05:08 +0100

    Remove redundant class.

--- interaction-core/src/main/java/com/temenos/interaction/core/resource/StreamResource.java
+++
@@ -1,9 +0,0 @@
-package com.temenos.interaction.core.resource;
-
-import java.io.OutputStream;
-
-public interface StreamResource {
-
-	public String getMIMEType();
-	public OutputStream getStream();
-}
