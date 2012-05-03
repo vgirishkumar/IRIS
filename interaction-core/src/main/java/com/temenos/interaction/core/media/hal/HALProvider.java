44b301dcf62e79ed4f49527b81828b066cbd0aa0
aphethean <aphethean@temenos.com>
2012-05-03 21:46:24 +0100

    Enable HAL provider to return application/hal+json.  Resolved a small issue with json generator by removing some unused jackson dependencies.  json generator now included as transitive dependency of halbuilder.

--- interaction-core/src/main/java/com/temenos/interaction/core/media/hal/HALProvider.java
+++ interaction-core/src/main/java/com/temenos/interaction/core/media/hal/HALProvider.java
@@ -142,6 +142,8 @@ public class HALProvider implements MessageBodyReader<RESTResource>, MessageBody
 		String representation = null;
 		if (halResource != null && mediaType.equals(com.temenos.interaction.core.media.hal.MediaType.APPLICATION_HAL_XML_TYPE)) {
 			representation = halResource.asRenderableResource().renderContent(ResourceFactory.HAL_XML);
+		} else if (halResource != null && mediaType.equals(com.temenos.interaction.core.media.hal.MediaType.APPLICATION_HAL_JSON_TYPE)) {
+			representation = halResource.asRenderableResource().renderContent(ResourceFactory.HAL_JSON);
 		} else {
 			throw new WebApplicationException(Response.Status.INTERNAL_SERVER_ERROR);
 		}
