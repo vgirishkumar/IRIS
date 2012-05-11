05355b948dee325011f5bee3f58302707db93b1d
aphethean <aphethean@temenos.com>
2012-05-11 17:33:01 +0100

    Added initial notes interaction to interaction-hateoas-simple example.

--- interaction-examples/interaction-hateoas-simple/src/main/java/com/temenos/interaction/example/hateoas/simple/Behaviour.java
+++ interaction-examples/interaction-hateoas-simple/src/main/java/com/temenos/interaction/example/hateoas/simple/Behaviour.java
@@ -1,9 +1,19 @@
 package com.temenos.interaction.example.hateoas.simple;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 import com.temenos.interaction.core.link.ResourceState;
 import com.temenos.interaction.core.link.ResourceStateMachine;
 
 public class Behaviour {
+
+	// the entity that generates new IDs
+	private final static String NEW_ENTITY_NAME = "ID";
+	// the entity that stores notes
+	private final static String NOTE_ENTITY_NAME = "note";
 
 	public ResourceState getInteractionModel() {
 		// this will be the service root
@@ -12,9 +22,41 @@ public class Behaviour {
 		ResourceState profile = new ResourceState("Profile", "profile", "/profile");
 		ResourceState preferences = new ResourceState("Preferences", "preferences", "/preferences");
 		
-		initialState.addTransition("GET", new ResourceStateMachine(profile));
+		initialState.addTransition("GET", profile);
 		initialState.addTransition("GET", new ResourceStateMachine(preferences));
+		initialState.addTransition("GET", getNotesInteractionModel());
 		return initialState;
+	}
+
+	public ResourceStateMachine getNotesInteractionModel() {
+		
+		ResourceState initialState = new ResourceState(NOTE_ENTITY_NAME, "initial", "/notes");
+		ResourceState newNoteState = new ResourceState(NEW_ENTITY_NAME, "new", "/notes/new");
+		ResourceState exists = new ResourceState(NOTE_ENTITY_NAME, "exists", "/notes/{id}");
+		ResourceState finalState = new ResourceState(NOTE_ENTITY_NAME, "end");
+
+		// a linkage map (target URI element, source entity element)
+		Map<String, String> uriLinkageMap = new HashMap<String, String>();
+
+		// a link from notes collection to create a new note (no arguments to this link)
+		initialState.addTransition("POST", newNoteState);		
+
+		// link from new note id to save the note
+		uriLinkageMap.clear();
+		uriLinkageMap.put("id", "lastId");
+		Set<String> relations = new HashSet<String>();
+		relations.add("_new");
+		newNoteState.addTransition("PUT", exists, uriLinkageMap);
+		
+		// a link on each note in the collection to delete the note
+		uriLinkageMap.clear();
+		uriLinkageMap.put("id", "noteID");
+//		initialState.addTransition("DELETE", exists);		
+
+		// update / delete note item (same linkage map)
+		exists.addTransition("PUT", exists, uriLinkageMap);		
+		exists.addTransition("DELETE", finalState, uriLinkageMap);
+		return new ResourceStateMachine(initialState);
 	}
 
 }
--- interaction-examples/interaction-hateoas-simple/src/main/resources/META-INF/responder_insert.sql
+++ interaction-examples/interaction-hateoas-simple/src/main/resources/META-INF/responder_insert.sql
@@ -4,3 +4,14 @@
 # For example:
 INSERT INTO `Profile`(`userID` , `name` , `DOB` , `location` , `pictureTaken`) VALUES('aphethean' , 'Aaron Phethean' , '1824-08-21 00:00:00' , '-27.4667,153.0333' , '2012-04-16 15:23:00');	
 INSERT INTO `Preferences`(`userID` , `currency` , `language`) VALUES('aphethean' , 'GBP' , 'EN');	
+
+#INSERT INTO `ID`(`DomainObjectName`, `LastId`) VALUES('NOTE', 1);
+
+INSERT INTO `Note`(`NoteID`,`Body`) VALUES(1,'Beverages');
+INSERT INTO `Note`(`NoteID`,`Body`) VALUES(2,'Condiments');
+INSERT INTO `Note`(`NoteID`,`Body`) VALUES(3,'Confections');
+INSERT INTO `Note`(`NoteID`,`Body`) VALUES(4,'Dairy Products');
+INSERT INTO `Note`(`NoteID`,`Body`) VALUES(5,'Grains/Cereals');
+INSERT INTO `Note`(`NoteID`,`Body`) VALUES(6,'Meat/Poultry');
+INSERT INTO `Note`(`NoteID`,`Body`) VALUES(7,'Produce');
+INSERT INTO `Note`(`NoteID`,`Body`) VALUES(8,'Seafood');
