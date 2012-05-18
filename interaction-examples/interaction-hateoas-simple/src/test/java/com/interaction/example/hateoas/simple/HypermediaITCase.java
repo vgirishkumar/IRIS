c8ab69c27392f981af045cee3d59ef72488f235f
aphethean <aphethean@temenos.com>
2012-05-18 14:45:46 +0100

    Added test to interaction-hateoas-simple for entry point links.

--- interaction-examples/interaction-hateoas-simple/src/test/java/com/interaction/example/hateoas/simple/HypermediaITCase.java
+++ interaction-examples/interaction-hateoas-simple/src/test/java/com/interaction/example/hateoas/simple/HypermediaITCase.java
@@ -2,6 +2,11 @@ package com.interaction.example.hateoas.simple;
 
 import static org.junit.Assert.*;
 
+import java.io.InputStreamReader;
+import java.util.List;
+
+import javax.ws.rs.core.Response;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,6 +15,9 @@ import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.test.framework.JerseyTest;
 import com.temenos.interaction.core.media.hal.MediaType;
+import com.theoryinpractise.halbuilder.ResourceFactory;
+import com.theoryinpractise.halbuilder.spi.Link;
+import com.theoryinpractise.halbuilder.spi.ReadableResource;
 
 /**
  * This test ensures that we can navigate from one application state
@@ -34,6 +42,31 @@ public class HypermediaITCase extends JerseyTest {
 	public void tearDown() {}
 
 
+	@Test
+	public void testGetEntryPointLinks() {
+		ClientResponse response = webResource.path("/").accept(MediaType.APPLICATION_HAL_JSON).get(ClientResponse.class);
+        assertEquals(Response.Status.Family.SUCCESSFUL, Response.Status.fromStatusCode(response.getStatus()).getFamily());
+
+		ResourceFactory resourceFactory = new ResourceFactory();
+		ReadableResource resource = resourceFactory.newResource(new InputStreamReader(response.getEntityInputStream()));
+
+		List<Link> links = resource.getLinks();
+		assertEquals(4, links.size());
+		for (Link link : links) {
+			if (link.getRel().equals("self")) {
+				assertEquals("http://localhost:8080/example/interaction-hateoas-simple.svc/", link.getHref());
+			} else if (link.getRel().equals("Preferences.preferences")) {
+				assertEquals("http://localhost:8080/example/interaction-hateoas-simple.svc/preferences", link.getHref());
+			} else if (link.getRel().equals("Profile.profile")) {
+				assertEquals("http://localhost:8080/example/interaction-hateoas-simple.svc/profile", link.getHref());
+			} else if (link.getRel().equals("note.initial")) {
+				assertEquals("http://localhost:8080/example/interaction-hateoas-simple.svc/notes", link.getHref());
+			} else {
+				fail("unexpected link");
+			}
+		}
+	}
+	
 	/**
 	 * Attempt a DELETE to the notes resource (a collection resource)
 	 */
