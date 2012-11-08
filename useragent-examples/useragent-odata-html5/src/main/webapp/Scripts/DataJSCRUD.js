5f8f371797fc7813cb4600caeeb14bd2bc684b12
aphethean <aphethean@temenos.com>
2012-11-08 07:31:12 +0000

    Initial revision of a generic html5 odata browser (useragent-odata-html5).  This initial commit includes the ability to list the service document, select a service, and display the list of entities in that entity set.

---
+++ useragent-examples/useragent-odata-html5/src/main/webapp/Scripts/DataJSCRUD.js
@@ -0,0 +1,356 @@
+﻿//ODATA Root Service URI
+var ODATA_SVC = "http://localhost:8080/example/interaction-odata-notes.svc/";
+var NOTES_ODATA_SVC = ODATA_SVC + "Note";
+
+//User edit form Variables
+var name = $("#name"),
+	email = $("#email"),
+	password = $("#password"),
+    allFields = $([]).add(name).add(email).add(password),
+	tips = $(".validateTips");
+
+
+//Page Load Actions
+
+function OnPageLoad() 
+{
+    $("#dialog:ui-dialog").dialog("destroy");
+
+    $("#dialog-form").dialog({
+        autoOpen: false,
+        height: 375,
+        width: 450,
+        modal: true,
+        close: function () {
+            allFields.val("").removeClass("ui-state-error");
+        }
+    });
+
+    $("#create-user").button()
+			.click(OpenCreateUserDialog);
+
+    GetServices();    
+} 
+
+//Page Events:
+
+//Gets all the services
+function GetServices() 
+{
+    $("#loadingServices").show();
+    OData.read({ 
+    		requestUri: ODATA_SVC,
+    		headers: { Accept: "application/atomsvc+xml" }
+    	}, GetServicesCallback);
+}
+//GetServices Success Callback
+function GetServicesCallback(data, request) 
+{
+    $("#loadingServices").hide();
+    $("#services").find("tr:gt(0)").remove();
+    alert("stop");
+    ApplyServiceTemplate(data.workspaces[0].collections);
+}
+
+//***********************Get Notes (READ)***************************
+//Gets all the Entities for an EntitySet
+function GetEntitySet(uri) 
+{
+    $("#loadingUsers").show();
+    OData.read({ 
+		requestUri: uri,
+		headers: { Accept: "application/atom+xml" }
+	}, GetEntitySetCallback);
+}
+
+//GetEntitySet Success Callback
+function GetEntitySetCallback(data, request) 
+{
+    $("#loadingUsers").hide();
+    $("#EntitySetName").text(data.__metadata.title)
+    $("#users").find("tr:gt(0)").remove();
+    ApplyTemplate(data.results)
+}
+
+//***********************End: Get Notes***************************
+
+//*****************************Add User (CREATE)***************************
+//Handle Create User Account button click
+function OpenCreateUserDialog() 
+{
+    $("#dialog-form").dialog("option", "title", "Create An Account");
+    $("#dialog-form").dialog("option", "buttons", [
+                                                            {
+                                                                text: "Save",
+                                                                click: function () {
+                                                                    var bValid = false;
+                                                                    bValid = ValidateUserData();
+                                                                    if (bValid) {
+                                                                        AddUser();
+                                                                    }
+                                                                }
+                                                            },
+                                                            {
+                                                                text: "Cancel",
+                                                                click: function () {
+                                                                    $("#dialog-form").dialog("close");
+                                                                }
+                                                            }
+                                                        ]
+                                    );
+    $("#dialog-form").dialog("open");
+}
+
+//Handle the DataJS call for new user acccount creation
+function AddUser() 
+{
+    $("#loading").show();
+    var newUserdata = { username: $("#name").val(), email: $("#email").val(), password: $("#password").val() };
+    var requestOptions = {
+        requestUri: USERS_ODATA_SVC,
+        method: "POST",
+        data: newUserdata
+    };
+
+    OData.request(requestOptions, AddSuccessCallback, AddErrorCallback);
+
+}
+
+//AddUser Success Callback
+function AddSuccessCallback(data, request) 
+{
+    $("#loading").hide('slow');
+    $("#dialog-form").dialog("close");
+    GetUsers();
+}
+
+//AddUser Error Callback
+function AddErrorCallback(error) 
+{
+    alert("Error : " + error.message)
+    $("#dialog-form").dialog("close");
+}
+
+//*************************End Add User***************************
+
+//*************************Update User (UPDATE)***************************
+//Handle Update hyper link click
+function OpenUpdateDialog(userId) 
+{
+    $("#loading").hide();
+    var cells = $("#userRow" + userId).children("td");
+    $("#name").val(cells.eq(0).text());
+    $("#email").val(cells.eq(1).text());
+    $("#password").val(cells.eq(2).text());
+
+    $("#dialog-form").dialog("option", "title", "Update Account");
+
+    $("#dialog-form").dialog("option", "buttons", [
+                        {
+                            text: "Save",
+                            click: function () {
+                                var bValid = false;
+                                bValid = ValidateUserData();
+                                if (bValid) {
+                                    UpdateUser(userId);
+                                }
+                            }
+                        },
+                        {
+                            text: "Cancel",
+                            click: function () {
+                                $("#dialog-form").dialog("close");
+                            }
+                        }
+                    ]);
+    $("#dialog-form").dialog("open");
+}
+
+//Handle DataJS calls to Update user data
+function UpdateUser(userId) 
+{
+    $("#loading").show();
+    var updateUserdata = { username: $("#name").val(), email: $("#email").val(), password: $("#password").val() };
+    var requestURI = USERS_ODATA_SVC + "(" + userId + ")";
+    var requestOptions = {
+        requestUri: requestURI,
+        method: "PUT",
+        data: updateUserdata
+    };
+
+    OData.request(requestOptions, UpdateSuccessCallback, UpdateErrorCallback);
+
+}
+
+//UpdateUser Suceess callback
+function UpdateSuccessCallback(data, request) {
+    $("#loading").hide('slow');
+    $("#dialog-form").dialog("close");
+    GetUsers();
+}
+
+//UpdateUser Error callback
+function UpdateErrorCallback(error) {
+    alert("Error : " + error.message)
+    $("#dialog-form").dialog("close");
+}
+//*************************End : Update User (UPDATE)***************************
+
+//*************************Delete User (DELETE)***************************
+
+var $dialog = null;
+
+//Handle Delete hyperlink click
+function OpenDeleteDialog(userId) 
+{
+    $("#loading").hide();
+    var cells = $("#userRow" + userId).children("td");
+
+    $dialog = $('<div></div>')
+		            .html('You are about to delete account "' + cells.eq(0).text() + '". Do you want to continue? ')
+		            .dialog({
+		                autoOpen: false,
+                        width:400,
+		                modal: true,
+		                buttons: {
+		                    "Yes": function () {
+		                        DeleteUser(userId);
+		                    },
+		                    "No": function () {
+		                        $(this).dialog("close");
+		                    }
+		                },
+		                title: 'Delete Account'
+		            });
+        $dialog.dialog('open');
+}
+
+//Handles DataJS calls for delete user
+function DeleteUser(userId) 
+{
+    var requestURI = USERS_ODATA_SVC + "(" + userId + ")";
+    var requestOptions = {
+                            requestUri: requestURI,
+                            method: "DELETE",
+                        };
+
+    OData.request(requestOptions, DeleteSuccessCallback, DeleteErrorCallback);
+}
+
+//DeleteUser Success callback
+function DeleteSuccessCallback()
+{
+    $dialog.dialog('close');
+    GetUsers();
+}
+
+//DeleteUser Error callback
+function DeleteErrorCallback(error)
+{
+    alert(error.message)
+}
+//*************************End : Delete User (DELETE)***************************
+
+//*************************Helper Functions***************************
+
+//Helper function to apply UI template for list of services
+function ApplyServiceTemplate(links) 
+{
+	for (obj in links) {
+		var link = links[obj];
+		var content = "<tr id=\"entitySet_" + link.title + "\">" +
+							"<td><a href=\"javascript:GetEntitySet('" + link.href + "')\">" + link.title + "</a></td>" +
+						"</tr>";
+        $(content).appendTo("#services tbody");
+	}
+}
+
+//Helper function to apply UI template for Notes
+function ApplyTemplate(data) 
+{
+	var header = "<tr class=\"ui-widget-header\">";
+	for (obj in data[0].__metadata.properties) {
+		header += "<th>" + obj + "</th>";
+	}
+	header += "<th>Actions</th>"
+	header += "</tr>";
+	$(header).appendTo("#users tbody");
+
+	var body = "";
+	for (row in data) {
+		if (row != "__metadata") {
+			body += "<tr id=\"userRow" + row + "\">";
+			for (obj in data[0].__metadata.properties) {
+				var prop = data[row][obj];
+				if (prop.__deferred != null && prop.__deferred.uri != null) {
+					body += "<td><a href=\"" + prop.__deferred.uri + "\">" + obj + "</a></td>";
+				} else {
+					body += "<td>" + prop + "</td>";
+				}
+			}
+			body += 
+				"<td>" +
+					"<a href=\"javascript:OpenUpdateDialog(${userid})\">Update</a>" +
+					" " +
+					"<a href=\"javascript:OpenDeleteDialog(${userid})\">Delete</a>" +
+					"</td>" +
+				"</tr>";
+		}                            
+	}
+    $(body).appendTo("#users tbody");
+}
+
+//Validation Helper, validates the user edit form
+function ValidateUserData() 
+{
+    var bValid = true;
+    allFields.removeClass("ui-state-error");
+
+    bValid = bValid && checkLength(name, "username", 3, 16);
+    bValid = bValid && checkLength(email, "email", 6, 80);
+    bValid = bValid && checkLength(password, "password", 5, 16);
+
+    bValid = bValid && checkRegexp(name, /^[a-z]([0-9a-z_])+$/i, "Username may consist of a-z, 0-9, underscores, begin with a letter.");
+    bValid = bValid && checkRegexp(email, /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i, "eg. ui@jquery.com");
+    bValid = bValid && checkRegexp(password, /^([0-9a-zA-Z])+$/, "Password field only allow : a-z 0-9");
+    return bValid;
+}
+
+//Helper function used to show validation errors
+function updateTips(t) 
+{
+    var tips = $(".validateTips");
+    tips
+				.text(t)
+				.addClass("ui-state-highlight");
+    setTimeout(function () {
+        tips.removeClass("ui-state-highlight", 1500);
+    },
+                        500);
+}
+
+//Helper function to validate length requirements
+function checkLength(o, n, min, max) {
+    if (o.val().length > max || o.val().length < min) {
+        o.addClass("ui-state-error");
+        updateTips("Length of " + n + " must be between " +
+					min + " and " + max + ".");
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+//Helper function to validate using regular expression
+function checkRegexp(o, regexp, n) {
+    if (!(regexp.test(o.val()))) {
+        o.addClass("ui-state-error");
+        updateTips(n);
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+//*************************End : Helper Functions***************************
---
+++ useragent-examples/useragent-odata-html5/src/main/webapp/index.jsp
@@ -0,0 +1,91 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title>DataJS CRUD Demo</title>
+    <link href="Styles/Site.css" rel="stylesheet" type="text/css" />
+    <link type="text/css" rel="Stylesheet" href="http://ajax.aspnetcdn.com/ajax/jquery.ui/1.8.10/themes/redmond/jquery-ui.css" /> 
+</head>
+<body>
+    
+    <div class="page">
+        <div class="header">
+            <div class="title">
+                <h1>
+                    DataJS CRUD Demo
+                </h1>
+            </div>
+            <div class="loginDisplay"></div>
+            <div class="clear hideSkiplink"></div>
+        </div>
+		<div class="leftCol">
+            <div id="service-root" class="ui-widget">
+	            <table id="services" class="ui-widget ui-widget-content">
+			            <tr class="ui-widget-header ">
+				            <th>EntitySet</th>
+			            </tr>
+	            </table>
+                <span id="loadingServices" style="display:none">Loading...</span>
+            </div>
+		</div>
+        <div class="main">
+            <div id="dialog-form" title="Create new user">
+	            <p class="validateTips">All form fields are required.</p>
+	            <form>
+	            <fieldset>
+		            <label for="name">Name</label>
+		            <input type="text" name="name" id="name" class="text ui-widget-content ui-corner-all" />
+		            <label for="email">Email</label>
+		            <input type="text" name="email" id="email" value="" class="text ui-widget-content ui-corner-all" />
+		            <label for="password">Password</label>
+		            <input type="password" name="password" id="password" value="" class="text ui-widget-content ui-corner-all" />
+                    <div id="loading" style="display:none">Loading...</div>
+	            </fieldset>
+	            </form>
+            </div>
+
+            <div id="users-contain" class="ui-widget">
+	            <h1 id="EntitySetName">Select EntitySet</h1>
+	            <table id="users" class="ui-widget ui-widget-content">
+                    <tr class="ui-widget-header "></tr>
+	            </table>
+                <button id="create-user">Create new user</button>
+                <span id="loadingUsers" style="display:none">Loading...</span>
+            </div>
+        </div>
+        <div class="clear"></div>
+        <div class="footer"></div>
+    </div>
+    <script type="text/javascript" src="http://ajax.aspnetcdn.com/ajax/jquery/jquery-1.5.1.min.js"></script>
+    <script type="text/javascript" src="http://ajax.aspnetcdn.com/ajax/jquery.ui/1.8.13/jquery-ui.min.js"></script>
+    <script type="text/javascript" src="Scripts/datajs-1.0.3.js"></script>
+    <script type="text/javascript" src="Scripts/jquery.tmpl.js"></script>
+    <script type="text/javascript" src="Scripts/DataJSCRUD.js"></script>
+    <script type="text/javascript">
+        $(OnPageLoad)
+    </script>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
