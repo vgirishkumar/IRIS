d299199b1d8c0b00615ae36d41ff84b49528b0c1
aphethean <aphethean@temenos.com>
2012-11-16 16:14:40 +0000

    Modify the examples and SDK to create a list of actions to pass into the ResourceState constructor.

--- interaction-examples/T24-ft-interaction-banking/src/main/java/com/temenos/interaction/example/hateoas/banking/Behaviour.java
+++ interaction-examples/T24-ft-interaction-banking/src/main/java/com/temenos/interaction/example/hateoas/banking/Behaviour.java
@@ -1,9 +1,9 @@
 package com.temenos.interaction.example.hateoas.banking;
 
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import com.temenos.interaction.core.hypermedia.Action;
 import com.temenos.interaction.core.hypermedia.CollectionResourceState;
@@ -14,8 +14,8 @@ public class Behaviour {
 
 	public ResourceState getInteractionModel() {
 		// this will be the service root
-		ResourceState initialState = new ResourceState("ServiceDocument", "begin", createActionSet(new Action("GETServiceDocument", Action.TYPE.VIEW), null), "/");
-		ResourceState preferences = new ResourceState("Preferences", "preferences", createActionSet(new Action("GETPreferences", Action.TYPE.VIEW), null), "/preferences");
+		ResourceState initialState = new ResourceState("ServiceDocument", "begin", createActionList(new Action("GETServiceDocument", Action.TYPE.VIEW), null), "/");
+		ResourceState preferences = new ResourceState("Preferences", "preferences", createActionList(new Action("GETPreferences", Action.TYPE.VIEW), null), "/preferences");
 		
 		initialState.addTransition("GET", preferences);
 		initialState.addTransition("GET", getFundsTransferInteractionModel());
@@ -23,15 +23,15 @@ public class Behaviour {
 	}
 
 	public ResourceStateMachine getFundsTransferInteractionModel() {
-		CollectionResourceState initialState = new CollectionResourceState("FundsTransfer", "initial", createActionSet(new Action("GETEntities", Action.TYPE.VIEW), null), "/fundstransfer");
-		ResourceState newFtState = new ResourceState(initialState, "new", createActionSet(new Action("T24Input", Action.TYPE.VIEW), null), "/new");
-		ResourceState viewed = new ResourceState("FundsTransfer", "view", createActionSet(new Action("GETEntity", Action.TYPE.VIEW), null), "/fundstransfer/{Id}", "Id");
-		ResourceState unauthorised = new ResourceState("FundsTransfer", "unauthorised", createActionSet(new Action("T24Input", Action.TYPE.VIEW), null), "/fundstransfer/{Id}/unauthorise", "Id");
-		ResourceState authorised = new ResourceState("FundsTransfer", "authorised", createActionSet(new Action("T24Authorise", Action.TYPE.VIEW), null), "/fundstransfer/{Id}/authorise", "Id");
-		ResourceState held = new ResourceState("FundsTransfer", "held", createActionSet(new Action("T24Hold", Action.TYPE.VIEW), null), "/fundstransfer/{Id}/hold", "Id");
-		ResourceState reversed = new ResourceState("FundsTransfer", "reversed", createActionSet(new Action("T24Reverse", Action.TYPE.VIEW), null), "/fundstransfer/{Id}/reverse", "Id");
-		ResourceState restored = new ResourceState("FundsTransfer", "restored", createActionSet(new Action("T24Restore", Action.TYPE.VIEW), null), "/fundstransfer/{Id}/restore", "Id");
-		ResourceState deleted = new ResourceState("FundsTransfer", "deleted", createActionSet(new Action("T24Delete", Action.TYPE.VIEW), null), "/fundstransfer/{Id}/delete", "Id");
+		CollectionResourceState initialState = new CollectionResourceState("FundsTransfer", "initial", createActionList(new Action("GETEntities", Action.TYPE.VIEW), null), "/fundstransfer");
+		ResourceState newFtState = new ResourceState(initialState, "new", createActionList(new Action("T24Input", Action.TYPE.VIEW), null), "/new");
+		ResourceState viewed = new ResourceState("FundsTransfer", "view", createActionList(new Action("GETEntity", Action.TYPE.VIEW), null), "/fundstransfer/{Id}", "Id");
+		ResourceState unauthorised = new ResourceState("FundsTransfer", "unauthorised", createActionList(new Action("T24Input", Action.TYPE.VIEW), null), "/fundstransfer/{Id}/unauthorise", "Id");
+		ResourceState authorised = new ResourceState("FundsTransfer", "authorised", createActionList(new Action("T24Authorise", Action.TYPE.VIEW), null), "/fundstransfer/{Id}/authorise", "Id");
+		ResourceState held = new ResourceState("FundsTransfer", "held", createActionList(new Action("T24Hold", Action.TYPE.VIEW), null), "/fundstransfer/{Id}/hold", "Id");
+		ResourceState reversed = new ResourceState("FundsTransfer", "reversed", createActionList(new Action("T24Reverse", Action.TYPE.VIEW), null), "/fundstransfer/{Id}/reverse", "Id");
+		ResourceState restored = new ResourceState("FundsTransfer", "restored", createActionList(new Action("T24Restore", Action.TYPE.VIEW), null), "/fundstransfer/{Id}/restore", "Id");
+		ResourceState deleted = new ResourceState("FundsTransfer", "deleted", createActionList(new Action("T24Delete", Action.TYPE.VIEW), null), "/fundstransfer/{Id}/delete", "Id");
 
 
 		Map<String, String> uriLinkageMap = new HashMap<String, String>();
@@ -63,8 +63,8 @@ public class Behaviour {
 		return new ResourceStateMachine(initialState);
 	}
 	
-	private Set<Action> createActionSet(Action view, Action entry) {
-		Set<Action> actions = new HashSet<Action>();
+	private List<Action> createActionList(Action view, Action entry) {
+		List<Action> actions = new ArrayList<Action>();
 		if (view != null)
 			actions.add(view);
 		if (entry != null)
