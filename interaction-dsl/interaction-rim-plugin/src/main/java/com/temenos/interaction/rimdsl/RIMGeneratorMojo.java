145f8a18bdb0091949010234f7c7c7ed0ac03673
aphethean <aphethean@temenos.com>
2012-11-20 14:28:39 +0000

    Initial implementation of interaction-rim-plugin.  A maven mojo (plugin) to generate the behaviour class from a RIM.  This commit has one basic test and some unanswered questions about the plugin dependencies.

---
+++ interaction-dsl/interaction-rim-plugin/src/main/java/com/temenos/interaction/rimdsl/RIMGeneratorMojo.java
@@ -0,0 +1,62 @@
+package com.temenos.interaction.rimdsl;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+import com.google.inject.Injector;
+import com.temenos.interaction.rimdsl.generator.launcher.Generator;
+
+import java.io.File;
+
+/**
+ * Goal which generates Java classes from a RIM.
+ *
+ * @goal generate
+ * @phase generate-sources
+ * @requiresDependencyResolution compile
+ */
+public class RIMGeneratorMojo extends AbstractMojo {
+    /**
+     * Location of the RIM file.
+     * @parameter
+     * @required
+     */
+    private File rimSourceFile;
+
+	/**
+     * Location of the RIM file.
+     * @parameter expression="${project.build.directory}/src-gen"
+     */
+    private File targetDirectory;
+    
+    public void setRimSourceFile(File rimSourceFile) {
+		this.rimSourceFile = rimSourceFile;
+	}
+
+
+	public void setTargetDirectory(File targetDirectory) {
+		this.targetDirectory = targetDirectory;
+	}
+    
+    public void execute() throws MojoExecutionException, MojoFailureException {
+		// check our configuration
+		if (rimSourceFile == null)
+			throw new MojoExecutionException("[rimSourceFile] not specified in plugin configuration");
+		if (targetDirectory == null) {
+			throw new MojoExecutionException("[targetDirectory] is set to null plugin configuration");
+		}
+
+		boolean ok = false;
+		if (!targetDirectory.exists()) {
+			getLog().info("Creating [targetDirectory] " + targetDirectory.toString());
+			targetDirectory.mkdirs();
+		}
+		Injector injector = new RIMDslStandaloneSetupGenerated().createInjectorAndDoEMFRegistration();
+		Generator generator = injector.getInstance(Generator.class);
+		ok = generator.runGenerator(rimSourceFile.toString(), targetDirectory.toString());
+
+		if (!ok)
+			throw new MojoFailureException("An unexpected error occurred while generating source");
+    }
+}
