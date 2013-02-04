c69f71706d4c1315679a9a0f9793b9e3dc352915
aphethean <aphethean@temenos.com>
2013-02-04 17:54:51 +0000

    Organise imports.

--- interaction-examples/interaction-odata-airline/src/main/java/com/interaction/example/odata/airline/model/Flight.java
+++ interaction-examples/interaction-odata-airline/src/main/java/com/interaction/example/odata/airline/model/Flight.java
@@ -1,13 +1,8 @@
 package com.interaction.example.odata.airline.model;
 
-import java.util.Collection;
-
 import javax.persistence.Basic;
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
