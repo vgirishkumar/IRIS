e6b7cdd0096e4ce7edfe4f9a07fd84d7cc711c06
aphethean <aphethean@temenos.com>
2013-12-18 15:28:00 +0000

    Remove the redundant T24 ft user agent example.

--- interaction-examples/T24-ft-interaction-banking/src/main/resources/META-INF/responder_insert.sql
+++
@@ -1,9 +0,0 @@
-#
-# Populate your responder inmemory database with some test values
-#
-# For example:
-INSERT INTO `Preferences`(`userID` , `currency` , `language`) VALUES('fred' , 'GBP' , 'EN');	
-
-INSERT INTO `FundTransfer`(`Id`,`Body`) VALUES(1, 'FT0001');
-INSERT INTO `FundTransfer`(`Id`,`Body`) VALUES(2, 'FT0002');
-INSERT INTO `FundTransfer`(`Id`,`Body`) VALUES(3, 'FT0003');
