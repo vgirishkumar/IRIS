267369e24f4041ee58cd822d51009054635418ac
ssethupathi <ssethupathi.s@gmail.com>
2014-01-27 10:20:38 +0000

    Duplicate ETAG setting line removed and setting accept language values moved into the block where http header is checked for null

--- interaction-core/src/main/java/com/temenos/interaction/core/rim/HTTPHypermediaRIM.java
+++ interaction-core/src/main/java/com/temenos/interaction/core/rim/HTTPHypermediaRIM.java
@@ -375,11 +375,6 @@ public class HTTPHypermediaRIM implements HTTPResourceInteractionModel {
     }
 	
 	private ResourceState initialiseInteractionContext(HttpHeaders headers, Event event, InteractionContext ctx, EntityResource<?> resource) {
-		//Apply the etag on the If-Match header if available
-		ctx.setPreconditionIfMatch(HeaderHelper.getFirstHeader(headers, HttpHeaders.IF_MATCH));
-		
-		ctx.setAcceptLanguage(HeaderHelper.getFirstHeader(headers, HttpHeaders.ACCEPT_LANGUAGE));
-
 		// set the resource for the commands to access
 		if(resource != null) {
         	ctx.setResource(resource);
@@ -389,6 +384,8 @@ public class HTTPHypermediaRIM implements HTTPResourceInteractionModel {
 		if (headers != null) {
 			//Apply the etag on the If-Match header if available
 			ctx.setPreconditionIfMatch(HeaderHelper.getFirstHeader(headers, HttpHeaders.IF_MATCH));
+
+			ctx.setAcceptLanguage(HeaderHelper.getFirstHeader(headers, HttpHeaders.ACCEPT_LANGUAGE));
 	    	// work out the target state and link used
 			LinkHeader linkHeader = null;
 			List<String> linkHeaders = headers.getRequestHeader("Link");
