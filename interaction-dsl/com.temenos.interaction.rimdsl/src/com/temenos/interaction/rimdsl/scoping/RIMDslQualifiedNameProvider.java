3e6154ebc71af8abca5a608f692e3b0cea61eceb
aphethean <aphethean@temenos.com>
2014-04-15 11:57:46 +0100

    Fix / complete implementation for RIMDslQualifiedNameProvider; closes #88

--- interaction-dsl/com.temenos.interaction.rimdsl/src/com/temenos/interaction/rimdsl/scoping/RIMDslQualifiedNameProvider.java
+++ interaction-dsl/com.temenos.interaction.rimdsl/src/com/temenos/interaction/rimdsl/scoping/RIMDslQualifiedNameProvider.java
@@ -4,9 +4,12 @@ import org.eclipse.emf.ecore.EObject;
 import org.eclipse.xtext.naming.IQualifiedNameProvider.AbstractImpl;
 import org.eclipse.xtext.naming.QualifiedName;
 
+import com.temenos.interaction.rimdsl.rim.Command;
 import com.temenos.interaction.rimdsl.rim.DomainDeclaration;
 import com.temenos.interaction.rimdsl.rim.Event;
+import com.temenos.interaction.rimdsl.rim.Relation;
 import com.temenos.interaction.rimdsl.rim.ResourceInteractionModel;
+import com.temenos.interaction.rimdsl.rim.State;
 
 /**
  * IRIS QualifiedNameProvider.
@@ -23,15 +26,37 @@ public class RIMDslQualifiedNameProvider extends AbstractImpl {
 
 	@Override
 	public QualifiedName getFullyQualifiedName(EObject obj) {
+		QualifiedName partialQualifiedName = null;
 		if (obj instanceof DomainDeclaration) {
 			DomainDeclaration d = (DomainDeclaration) obj;
 			return QualifiedName.create(d.getName());
+		} else if (obj instanceof ResourceInteractionModel) {
+			ResourceInteractionModel r = (ResourceInteractionModel) obj;
+			partialQualifiedName = QualifiedName.create(r.getName());
+		} else if (obj instanceof State) {
+			State s = (State) obj;
+			partialQualifiedName = QualifiedName.create(s.getName());
 		} else if (obj instanceof Event) {
 			Event e = (Event) obj;
-			ResourceInteractionModel parent = (ResourceInteractionModel) e.eContainer();
-			return QualifiedName.create(parent.getName(), e.getName());
-		// TODO more ... } else if (obj instanceof Event) {
+			return QualifiedName.create(e.getName());
+		} else if (obj instanceof Command) {
+			Command c = (Command) obj;
+			return QualifiedName.create(c.getName());
+		} else if (obj instanceof Relation) {
+			Relation r = (Relation) obj;
+			return QualifiedName.create(r.getName());
 		}
+		
+		if (partialQualifiedName != null) {
+			EObject temp = obj;
+			while (temp.eContainer() != null) {
+				temp = temp.eContainer();
+				QualifiedName parentsQualifiedName = getFullyQualifiedName(temp);
+				if (parentsQualifiedName != null)
+					return parentsQualifiedName.append(partialQualifiedName);
+			}
+		}
+		
 		return null;
 	}
 
