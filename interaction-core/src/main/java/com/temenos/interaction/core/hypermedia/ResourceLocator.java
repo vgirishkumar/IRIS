29020a7e12ebb5d05740a8f113dc985b501d624b
Martin Lambert <mlambert@temenos.com>
2014-07-07 15:13:35 +0100

    Adding support for dynamic resource resolution.

---
+++ interaction-core/src/main/java/com/temenos/interaction/core/hypermedia/ResourceLocator.java
@@ -0,0 +1,42 @@
+package com.temenos.interaction.core.hypermedia;
+
+/*
+ * #%L
+ * interaction-core
+ * %%
+ * Copyright (C) 2012 - 2014 Temenos Holdings N.V.
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+
+
+/**
+ * Implementations of this interface provide a way of resolving an alias to a resource state name 
+ *
+ * @author mlambert
+ *
+ */
+public interface ResourceLocator {
+	/**
+	 * 
+	 * @param id
+	 * 			The identifier of the selected record
+	 * @param alias
+	 * 			The alias to resolve to a resource state name
+	 * 
+	 * @return
+	 */
+	ResourceState resolve(Object id, String... alias);
+}
