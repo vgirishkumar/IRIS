49ceeb6fe1cef3299def0916d01ea4e9c1f56116
Andres C. Burgos <burgosandres@gmail.com>
2015-11-25 10:52:49 +0000

    ParentLastURLClassloader added.

---
+++ interaction-dynamic-loader/src/main/java/com/temenos/interaction/loader/classloader/NonDelegatingClassloader.java
@@ -0,0 +1,61 @@
+package com.temenos.interaction.loader.classloader;
+
+/*
+ * #%L
+ * interaction-dynamic-loader
+ * %%
+ * Copyright (C) 2012 - 2015 Temenos Holdings N.V.
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+ 
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.net.URLStreamHandlerFactory;
+
+public class NonDelegatingClassloader extends URLClassLoader {
+ 
+    public NonDelegatingClassloader(URL[] urls, ClassLoader cl, URLStreamHandlerFactory urlshf) {
+        super(urls, cl, urlshf);
+    }
+ 
+    public NonDelegatingClassloader(URL[] urls, ClassLoader cl) {
+        super(urls, cl);
+    }
+ 
+    public NonDelegatingClassloader(URL[] urls) {
+        super(urls);
+    }
+ 
+    @Override
+    protected synchronized Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException {
+        // check if loaded locally
+        Class<?> result = findLoadedClass(name);
+        if (result == null) {
+            try {
+                // search locally
+                result = findClass(name);
+            } catch (ClassNotFoundException ex) {
+                // Next, delegate to the parent, if not found locally.
+                result = getParent().loadClass(name);
+            }
+        }
+ 
+        if (resolve) {
+            resolveClass(result);
+        }
+        return result;
+    }
+}
