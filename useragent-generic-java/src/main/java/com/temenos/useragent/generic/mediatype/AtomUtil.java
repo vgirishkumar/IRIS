69d601b7051c0cf57372ec31d731a18d353de8b1
ssethupathi <ssethupathi@temenos.com>
2016-04-12 09:16:51 +0100

    Fixing getCount and getValue on AtomEntryHandler for complex properties

--- useragent-generic-java/src/main/java/com/temenos/useragent/generic/mediatype/AtomUtil.java
+++ useragent-generic-java/src/main/java/com/temenos/useragent/generic/mediatype/AtomUtil.java
@@ -35,8 +35,8 @@ public class AtomUtil {
 	public final static String NS_ODATA_METADATA = "http://schemas.microsoft.com/ado/2007/08/dataservices/metadata";
 	public final static String NS_ODATA_RELATED = "http://schemas.microsoft.com/ado/2007/08/dataservices/related";
 
-	public final static String REGX_VALID_ELEMENT = "[a-zA-Z0-9_]+(\\(\\d+\\))*";
-	public final static String REGX_ELEMENT_WITH_INDEX = "[a-zA-Z0-9_]+(\\(\\d+\\))+";
+	public final static String REGX_VALID_ELEMENT = "[a-zA-Z0-9_]*";
+	public final static String REGX_VALID_PART_WITH_INDEX = "[a-zA-Z0-9_]+(\\(\\d+\\))+";
 
 	public static String extractRel(String relAttributeValue) {
 		int spaceIndex = relAttributeValue.trim().indexOf(" ");
