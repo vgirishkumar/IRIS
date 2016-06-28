ebc03026dc7fa33bac3cca57f93f9d344393f343
cmclopes <carlos.lopes.est@gmail.com>
2016-06-28 15:05:27 +0100

    Code clean on interaction-examples: interaction-mashup-streaming

--- interaction-examples/interaction-mashup-streaming/src/main/java/com/temenos/interaction/example/mashup/streaming/MD5Util.java
+++ interaction-examples/interaction-mashup-streaming/src/main/java/com/temenos/interaction/example/mashup/streaming/MD5Util.java
@@ -22,12 +22,18 @@ package com.temenos.interaction.example.mashup.streaming;
  */
 
 
-import java.io.*;
-import java.security.*;
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MD5Util {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MD5Util.class);
+    
 	public static String hex(byte[] array) {
-		StringBuffer sb = new StringBuffer();
+		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < array.length; ++i) {
 			sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100).substring(
 					1, 3));
@@ -40,7 +46,9 @@ public class MD5Util {
 			MessageDigest md = MessageDigest.getInstance("MD5");
 			return hex(md.digest(message.getBytes("CP1252")));
 		} catch (NoSuchAlgorithmException e) {
+		    LOGGER.error("Failed to find a the algorithm for MessageDigest.", e);
 		} catch (UnsupportedEncodingException e) {
+		    LOGGER.error("Unsupported encoding.", e);
 		}
 		return null;
 	}
