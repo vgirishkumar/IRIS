069622b1d0bf019c90f11c5b8321c6874edb1d2b
Sethupathi Sivasamy <ssethupathi@temenos.com>
2016-10-19 12:06:18 +0530

    Fixing a failing unit test with empty string as property name

--- useragent-generic-java/src/test/java/com/temenos/useragent/generic/mediatype/PropertyNameUtilTest.java
+++ useragent-generic-java/src/test/java/com/temenos/useragent/generic/mediatype/PropertyNameUtilTest.java
@@ -1,5 +1,27 @@
 package com.temenos.useragent.generic.mediatype;
 
+/*
+ * #%L
+ * useragent-generic-java
+ * %%
+ * Copyright (C) 2012 - 2016 Temenos Holdings N.V.
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+
+
 import static org.junit.Assert.*;
 
 import org.junit.Test;
@@ -52,8 +74,12 @@ public class PropertyNameUtilTest {
 		assertEquals("bar(1)", parts[1]);
 		assertEquals("blah", parts[2]);
 
-		parts = PropertyNameUtil.flattenPropertyName("");
-		assertEquals(0, parts.length);
+		try {
+			parts = PropertyNameUtil.flattenPropertyName("");
+			fail("IllegalArgumentException should be thrown");
+		} catch (Exception e) {
+			assertTrue(e instanceof IllegalArgumentException);
+		}
 
 		try {
 			PropertyNameUtil.flattenPropertyName(null);
